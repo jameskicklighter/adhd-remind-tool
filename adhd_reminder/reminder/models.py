921439f68f76a79ba77889dc440f2b3cc3070627
Julius Liu <codereview@juliusliu.dev>
2024-03-25 22:05:25 +0800

    Added two models.

--- adhd_reminder/reminder/models.py
+++ adhd_reminder/reminder/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    pass
+
+class Task(models.Model):
+    title = models.CharField(max_length=64)
+    note = models.CharField(max_length=256)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
+
+    # Define constants for priority
+    HIGH = 1
+    MID = 2
+    LOW = 3
+    priorities = (
+        (HIGH, "!!!"),
+        (MID, "!!"),
+        (LOW, "!")
+    )
+    # Pass in None for this field in order to save it as NULL.
+    priority = models.IntegerField(choices=priorities, null=True, blank=True)
