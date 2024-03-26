f9236d37eab25d4fde8cc96017e86047e74b1274
Julius Liu <codereview@juliusliu.dev>
2024-03-26 13:24:49 +0800

    Modified templates

--- adhd_reminder/reminder/views.py
+++ adhd_reminder/reminder/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, "reminder/layout.html")
+    return render(request, "reminder/index.html")
     # return HttpResponse("<h1>Hi! This is our first project together :). Hope you enjoy. </h1>")
