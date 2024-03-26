393cee3747640c6aff7de33f8192c3653da354bc
jameskicklighter <103899871+jameskicklighter@users.noreply.github.com>
2024-03-25 21:54:44 -0700

    Basic Layout Template
    Added basic HTML layout template without forms.

--- adhd_reminder/reminder/views.py
+++ adhd_reminder/reminder/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Hi! This is our first project together :). Hope you enjoy. </h1>")
+    return render(request, "reminder/layout.html")
+    # return HttpResponse("<h1>Hi! This is our first project together :). Hope you enjoy. </h1>")
